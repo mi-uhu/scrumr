873aeae90c61b04232e3843e704f5195092a3bac
Ermin Kusur <11904648@edu.campus02.at>
2023-03-10 15:41:00 +0100

    board, sidebar, sample login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { FooterComponent } from './footer/footer.component';
 import { RoutingModule } from './routing/routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
-    declarations: [AppComponent, NavbarComponent, WebPresenceComponent, FooterComponent, LoginComponent, SignupComponent],
+    declarations: [AppComponent, NavbarComponent, WebPresenceComponent, FooterComponent, LoginComponent, SignupComponent, SidebarComponent, BoardComponent],
     imports: [BrowserModule, RoutingModule],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { WebPresenceComponent } from '../web-presence/web-presence.component';
 import { LoginComponent } from '../login/login.component';
 import { SignupComponent } from '../signup/signup.component';
+import { BoardComponent } from '../board/board.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', component: WebPresenceComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'board', component: BoardComponent },
     { path: '**', component: WebPresenceComponent }, //TODO: not found component
 ];
 
