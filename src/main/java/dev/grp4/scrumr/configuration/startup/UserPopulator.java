31762955471a8ac4ee0f25cbe71bc6ee250779ad
michael <michael.unterhuber94@gmail.com>
2023-03-11 19:41:54 +0100

    uncommented user classes

--- src/main/java/dev/grp4/scrumr/configuration/startup/UserPopulator.java
+++ src/main/java/dev/grp4/scrumr/configuration/startup/UserPopulator.java
@@ -1,40 +1,46 @@
-//package dev.grp4.scrumr.configuration.startup;
-//
-//import dev.grp4.scrumr.auth.Roles;
-//import dev.grp4.scrumr.user.User;
-//import dev.grp4.scrumr.user.UserGrantedAuthority;
-//import dev.grp4.scrumr.user.UserGrantedAuthorityRepo;
-//import dev.grp4.scrumr.user.UserRepo;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.Collections;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class UserPopulator implements CommandLineRunner {
-//
-//  private final UserRepo userRepo;
-//  private final UserGrantedAuthorityRepo userGrantedAuthorityRepo;
-//  private final PasswordEncoder passwordEncoder;
-//
-//  @Override
-//  public void run(String... args) throws Exception {
-//    User user = new User();
-//    user.setUsername("username");
-//    user.setPassword(passwordEncoder.encode("password"));
-//    user.setEnabled(true);
-//    user.setCredentialsNonExpired(true);
-//    user.setAccountNonExpired(true);
-//    user.setAccountNonLocked(true);
-//
-//    UserGrantedAuthority grantedAuthority = new UserGrantedAuthority();
-//    grantedAuthority.setAuthority(Roles.USER);
-//    grantedAuthority.setUser(user);
-//    userRepo.save(user);
-//    userGrantedAuthorityRepo.save(grantedAuthority);
-//    user.setAuthorities(Collections.singleton(grantedAuthority));
-//  }
-//}
+package dev.grp4.scrumr.configuration.startup;
+
+import dev.grp4.scrumr.auth.Roles;
+import dev.grp4.scrumr.user.User;
+import dev.grp4.scrumr.user.UserGrantedAuthority;
+import dev.grp4.scrumr.user.UserGrantedAuthorityRepo;
+import dev.grp4.scrumr.user.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class UserPopulator implements CommandLineRunner {
+
+  private final UserRepo userRepo;
+  private final UserGrantedAuthorityRepo userGrantedAuthorityRepo;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  public void run(String... args) throws Exception {
+    populate("username", "password", Roles.USER);
+    populate("user2", "pass123!", Roles.USER);
+  }
+
+  private void populate(String username, String password, String roles) {
+    User user = new User();
+    user.setUsername(username);
+    user.setPassword(passwordEncoder.encode(password));
+    user.setEnabled(true);
+    user.setCredentialsNonExpired(true);
+    user.setAccountNonExpired(true);
+    user.setAccountNonLocked(true);
+
+    UserGrantedAuthority grantedAuthority = new UserGrantedAuthority();
+    grantedAuthority.setAuthority(roles);
+    grantedAuthority.setUser(user);
+    userRepo.save(user);
+    userGrantedAuthorityRepo.save(grantedAuthority);
+    user.setAuthorities(Collections.singleton(grantedAuthority));
+
+  }
+}
--- src/main/java/dev/grp4/scrumr/user/UserServiceImpl.java
+++ src/main/java/dev/grp4/scrumr/user/UserServiceImpl.java
@@ -1,49 +1,49 @@
-//package dev.grp4.scrumr.user;
-//
-//import jakarta.transaction.Transactional;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@RequiredArgsConstructor
-//@Service
-//@Transactional
-//@Slf4j
-//public class UserServiceImpl implements UserService {
-//
-//  private final UserRepo userRepo;
-//
+package dev.grp4.scrumr.user;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+  private final UserRepo userRepo;
+
 //  @Override
-//  public User loadUserByUsername(String username) throws UsernameNotFoundException {
-//    log.info( "Fetching user by username='{}'", username);
-//    return userRepo.findByUsername(username)
-//      .orElseThrow( () -> new UsernameNotFoundException("Username '" + username + "' not found.") );
-//  }
-//
-//  @Override
-//  public User get(long userId) {
-//    log.info("Fetching user by id='{}'", userId);
-//    return userRepo.findById(userId).orElseThrow();
-//  }
-//
-//  @Override
-//  public void create(User user) {
-//    log.info("Create user username='{}'", user.getUsername());
-//    userRepo.save(user);
-//  }
-//
-//  @Override
-//  public void update(User user) {
-//    log.info("Update user id='{}' username='{}'", user.getUserId(), user.getUsername());
-//    userRepo.save(user);
-//
-//  }
-//
-//  @Override
-//  public boolean delete(long userId) {
-//    log.info("Delete user id='{}'", userId);
-//    userRepo.deleteById(userId);
-//    return true;
-//  }
-//}
+  public User loadUserByUsername(String username) throws UsernameNotFoundException {
+    log.info( "Fetching user by username='{}'", username);
+    return userRepo.findByUsername(username)
+      .orElseThrow( () -> new UsernameNotFoundException("Username '" + username + "' not found.") );
+  }
+
+  @Override
+  public User get(long userId) {
+    log.info("Fetching user by id='{}'", userId);
+    return userRepo.findById(userId).orElseThrow();
+  }
+
+  @Override
+  public void create(User user) {
+    log.info("Create user username='{}'", user.getUsername());
+    userRepo.save(user);
+  }
+
+  @Override
+  public void update(User user) {
+    log.info("Update user id='{}' username='{}'", user.getUserId(), user.getUsername());
+    userRepo.save(user);
+
+  }
+
+  @Override
+  public boolean delete(long userId) {
+    log.info("Delete user id='{}'", userId);
+    userRepo.deleteById(userId);
+    return true;
+  }
+}
